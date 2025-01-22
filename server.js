56504df0e9de95cf43f985e8e1a3a7c102e6eaf9
rashirawal22 <rashi.rawal@kalvium.community>
2025-01-22 12:16:47 +0530

    updated

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-const express = required('express');
+const express = require('express');
 const app = express();
 
-app.use(express.json)
 
-app.listengit 
+
+
 
 const user = [
     {id:1, name: 'rashi', age:13}
 ]//creating object array 
 
 app.get('/api/getUser', (req, res) => {
-    res.send(user);
+    res.json(user);
 }); 
 
-app.listen
+app.listen(8000, () => {
+    console.log('server is running on port $(port)')
+})
+
